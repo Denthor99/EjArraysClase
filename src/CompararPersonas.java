4d0f19985de6e96e96d24e6b6c95f3c15614f50d
Den <drodsan2708@g.educaand.es>
2023-01-25 10:37:04 +0100

    drodsan2708 - Ejercicios de clase - 25/01/2023

--- src/CompararPersonas.java
+++ src/CompararPersonas.java
@@ -1,8 +1,8 @@
 public class CompararPersonas {
     public static void main(String[] args) {
-        PersonaArray a=new PersonaArray();
+        PersonaArray a=new PersonaArray("a");
         a.nombre="a";
-        PersonaArray b=new PersonaArray();
+        PersonaArray b=new PersonaArray("a");
         b.nombre="b";
 
         System.out.println(a==b);
